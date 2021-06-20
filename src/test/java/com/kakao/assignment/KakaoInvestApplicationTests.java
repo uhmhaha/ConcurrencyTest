2ae979b2d23a804433f7198db7dac09b0fd1c021
skyang <uhmhaha@nate.com>
2021-06-21 00:05:38 +0900

    sub final

--- src/test/java/com/kakao/assignment/KakaoInvestApplicationTests.java
+++ src/test/java/com/kakao/assignment/KakaoInvestApplicationTests.java
@@ -1,5 +1,23 @@
 package com.kakao.assignment;
 
-class KakaoInvestApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.kakao.assignment.controller.InvestController;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class KakaoInvestApplicationTests {
+	
 }
